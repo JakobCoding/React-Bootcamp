6230da57ac64063ec6019732eb74e1d9ed8d7e67
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-05 19:30:42 +0800

    Learning import and export syntax in JS

---
+++ JavaScript-Refresher/app.js
@@ -0,0 +1,14 @@
+// import { apiKey } from "./util.js";
+// import { myName } from "./util.js";
+// import { course } from "./util.js";
+
+// importing default from util.js
+// import apiKey from "./util.js"
+
+// console.log(apiKey)
+
+// console.log(apiKey, myName, course);
+
+// import * as util from "./util.js";
+// console.log(util.apiKey);
+
---
+++ JavaScript-Refresher/util.js
@@ -0,0 +1,7 @@
+// export let apiKey = "here is the Api Key";
+// export let myName = "Jake is learning React";
+// export let course = "On Udemy";
+
+// export default is used when you only have one function to export import which is common in React
+
+// export default "here is the Api key Jake is learning React on Udemy";
